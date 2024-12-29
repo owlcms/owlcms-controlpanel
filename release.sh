c9673b0ea3058f9c2b1f1152f56060cfde5d72df
Jean-François Lamy <jf@fjlamy.dev>
2024-12-28 21:40:34 -0500

    automated script

--- release.sh
+++ release.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 export TAG=1.0.1
 
-fyne-cross windows -arch amd64 -app-id app.owlcms.owlcms-launcher -icon Icon.png -name owlcms-windows
-fyne-cross linux -arch amd64 -app-id app.owlcms.owlcms-launcher -icon Icon.png -name owlcms-linux
-fyne-cross linux -arch arm64 -app-id app.owlcms.owlcms-launcher -icon Icon.png -name owlcms-pi
+# fyne-cross windows -arch amd64 -app-id app.owlcms.owlcms-launcher -icon Icon.png -name owlcms-windows
+# fyne-cross linux -arch amd64 -app-id app.owlcms.owlcms-launcher -icon Icon.png -name owlcms-linux
+# fyne-cross linux -arch arm64 -app-id app.owlcms.owlcms-launcher -icon Icon.png -name owlcms-pi
 
-cp fyne-cross/bin/linux-arm64/owlcms-launcher fyne-cross/bin/linux-arm64/owlcms-pi
-cp fyne-cross/bin/linux-amd64/owlcms-launcher fyne-cross/bin/linux-amd64/owlcms-linux
+# cp fyne-cross/bin/linux-arm64/owlcms-launcher fyne-cross/bin/linux-arm64/owlcms-pi
+# cp fyne-cross/bin/linux-amd64/owlcms-launcher fyne-cross/bin/linux-amd64/owlcms-linux
 
-git add --all
-git commit -m "$TAG"
-git push
+# git add --all
+# git commit -m "$TAG"
+# git push
 
 gh release delete $TAG -y
-gh release create $TAG --notes-file ../RELEASE.md -t "owlcms-launcher $TAG"
+gh release create $TAG --notes-file RELEASE.md -t "owlcms-launcher $TAG"
 gh release upload $TAG fyne-cross/bin/linux-arm64/owlcms-pi
 gh release upload $TAG fyne-cross/bin/linux-amd64/owlcms-linux
 gh release upload $TAG fyne-cross/bin/windows-amd64/owlcms-windows.exe
