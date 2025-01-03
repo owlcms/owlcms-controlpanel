c5059d145f1274a3509e36fd907b0bb88c483f78
Jean-François Lamy <jf@fjlamy.dev>
2025-01-03 17:36:33 -0500

    1.3.0

--- release.sh
+++ release.sh
@@ -3,22 +3,23 @@ export TAG=1.3.0
 git pull
 rm -f owlcms-launcher*
 
-# fyne-cross windows -arch amd64 -app-id app.owlcms.owlcms-launcher -icon Icon.png -name owlcms
-# fyne-cross linux -arch amd64 -app-id app.owlcms.owlcms-launcher -icon Icon.png -name owlcms
-# fyne-cross linux -arch arm64 -app-id app.owlcms.owlcms-launcher -icon Icon.png -name owlcms
+fyne-cross windows -arch amd64 -app-id app.owlcms.owlcms-launcher -icon Icon.png -name owlcms
+fyne-cross linux -arch amd64 -app-id app.owlcms.owlcms-launcher -icon Icon.png -name owlcms
+fyne-cross linux -arch arm64 -app-id app.owlcms.owlcms-launcher -icon Icon.png -name owlcms
 
-# cp fyne-cross/bin/linux-arm64/owlcms-launcher fyne-cross/bin/linux-arm64/owlcms-pi
-# cp fyne-cross/bin/linux-amd64/owlcms-launcher fyne-cross/bin/linux-amd64/owlcms-linux
+cp fyne-cross/bin/linux-arm64/owlcms-launcher fyne-cross/bin/linux-arm64/owlcms-pi
+cp fyne-cross/bin/linux-amd64/owlcms-launcher fyne-cross/bin/linux-amd64/owlcms-linux
 
-# fpm -s tar -t deb -n owlcms-launcher -v ${TAG} -a arm64 --prefix / --after-install ./dist/after_install.sh --after-remove ./dist/after_remove.sh ./fyne-cross/dist/linux-arm64/owlcms.tar.xz 
-# fpm -s tar -t deb -n owlcms-launcher -v ${TAG} -a amd64 --prefix / --after-install ./dist/after_install.sh --after-remove ./dist/after_remove.sh ./fyne-cross/dist/linux-amd64/owlcms.tar.xz
-# mv owlcms-launcher_${TAG}_arm64.deb owlcms-launcher_${TAG}_pi.deb
+fpm -s tar -t deb -n owlcms-launcher -v ${TAG} -a arm64 --prefix / --after-install ./dist/after_install.sh --after-remove ./dist/after_remove.sh ./fyne-cross/dist/linux-arm64/owlcms.tar.xz 
+fpm -s tar -t deb -n owlcms-launcher -v ${TAG} -a amd64 --prefix / --after-install ./dist/after_install.sh --after-remove ./dist/after_remove.sh ./fyne-cross/dist/linux-amd64/owlcms.tar.xz
+mv owlcms-launcher_${TAG}_arm64.deb owlcms-launcher_${TAG}_pi.deb
 
-rm -f /tmp/RELEASE.md
-cp
+
+# inexplicable behavior on wsl2, gh cannot stat tmp file for RELEASE.md
+cp RELEASE.md /tmp
 sed -i "s/_TAG_/${TAG}/g" ./RELEASE.md
 cp /tmp/RELEASE.md .
-gh release list
+
 
 git add --all
 git commit -m "${TAG}"
