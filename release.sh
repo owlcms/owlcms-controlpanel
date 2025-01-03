c722f9012fd8f52ce3bcd1c1a436965f9b756183
Jean-François Lamy <jf@fjlamy.dev>
2025-01-03 17:19:22 -0500

    1.3.0

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 export TAG=1.3.0
 git pull
 rm -f owlcms-launcher*
@@ -14,7 +14,7 @@ fpm -s tar -t deb -n owlcms-launcher -v ${TAG} -a arm64 --prefix / --after-insta
 fpm -s tar -t deb -n owlcms-launcher -v ${TAG} -a amd64 --prefix / --after-install ./dist/after_install.sh --after-remove ./dist/after_remove.sh ./fyne-cross/dist/linux-amd64/owlcms.tar.xz
 mv owlcms-launcher_${TAG}_arm64.deb owlcms-launcher_${TAG}_pi.deb
 
-sed "s/_TAG_/${TAG}/g" RELEASE.md > /tmp/RELEASE.md
+sed "s/_TAG_/${TAG}/g" ./RELEASE.md > /tmp/RELEASE.md
 
 git add --all
 git commit -m "${TAG}"
