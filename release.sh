b473d4a905bda8890bf57308412fd1a45ef38a6e
Jean-François Lamy <jf@fjlamy.dev>
2025-01-09 11:32:17 -0500

    sync

--- release.sh
+++ release.sh
@@ -2,6 +2,7 @@
 export TAG=v1.6.4
 export DEB_TAG=${TAG#v}
 git pull
-git commit -am "Release $TAG prepared."
+git commit -am "Release $TAG."
+git push
 git tag -a ${TAG} -m "Release $TAG"
 git push origin --tags
