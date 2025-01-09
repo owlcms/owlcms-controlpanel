207a713d1574b44225bcfe685ad084db7ec72e1c
Jean-François Lamy <jf@fjlamy.dev>
2025-01-09 11:30:18 -0500

    Release v1.6.4 prepared.

--- release.sh
+++ release.sh
@@ -1,30 +1,7 @@
 #!/bin/bash -x
-export TAG=v1.5.2
+export TAG=v1.6.4
 export DEB_TAG=${TAG#v}
 git pull
-rm -f owlcms-launcher*
-
-fyne-cross windows -arch amd64 -app-id app.owlcms.owlcms-launcher -icon Icon.png -name owlcms
-fyne-cross linux -arch amd64 -app-id app.owlcms.owlcms-launcher -icon Icon.png -name owlcms
-fyne-cross linux -arch arm64 -app-id app.owlcms.owlcms-launcher -icon Icon.png -name owlcms
-
-cp fyne-cross/bin/linux-arm64/owlcms-launcher fyne-cross/bin/linux-arm64/owlcms-pi
-cp fyne-cross/bin/linux-amd64/owlcms-launcher fyne-cross/bin/linux-amd64/owlcms-linux
-
-fpm -s tar -t deb -n owlcms-launcher -v ${DEB_TAG} -a arm64 --prefix / --after-install ./dist/after_install.sh --after-remove ./dist/after_remove.sh ./fyne-cross/dist/linux-arm64/owlcms.tar.xz 
-fpm -s tar -t deb -n owlcms-launcher -v ${DEB_TAG} -a amd64 --prefix / --after-install ./dist/after_install.sh --after-remove ./dist/after_remove.sh ./fyne-cross/dist/linux-amd64/owlcms.tar.xz
-mv owlcms-launcher_${DEB_TAG}_arm64.deb owlcms-launcher_${DEB_TAG}_pi.deb
-
-# gh requires data from the current repo
-cp RELEASE.md ./dist/RELEASE.md
-sed -i "s/_TAG_/${TAG}/g" ./dist/RELEASE.md
-
-# gh release delete ${TAG} -y
-gh release edit ${TAG} --notes-file ./dist/RELEASE.md --title "owlcms-launcher ${TAG}"
-gh release upload ${TAG} owlcms-launcher_${DEB_TAG}_pi.deb
-gh release upload ${TAG} owlcms-launcher_${DEB_TAG}_amd64.deb
-gh release upload ${TAG} fyne-cross/bin/linux-arm64/owlcms-pi
-gh release upload ${TAG} fyne-cross/bin/linux-amd64/owlcms-linux
-gh release upload ${TAG} fyne-cross/bin/windows-amd64/owlcms.exe
-
-git fetch --tags
+git commit -am "Release $TAG prepared."
+git tag -a ${TAG} -m "Release $TAG"
+git push origin --tags
