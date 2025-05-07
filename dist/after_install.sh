4f6053d32810006e2215754bb81d5cfdb888ba27
Jean-François Lamy <jf@jflamy.dev>
2025-05-07 17:48:13 -0400

    fix shortcut generation on rpi

--- dist/after_install.sh
+++ dist/after_install.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 if [[ -n "$SUDO_USER" ]]; then
   user_home=$(getent passwd "$SUDO_USER" | cut -d: -f6)
-  cp /usr/local/share/applications/owlcms.desktop $user_home/Desktop/owlcms.desktop
+  echo >> $user_home/Desktop/owlcms.desktop <<EOF
+[Desktop Entry]
+Type=Link
+Name=owlcms
+Icon=owlcms
+URL=/usr/local/share/applications/owlcms.desktop
+EOF
+  chmod +x $user_home/Desktop/owlcms.desktop
 else
   if [[ -n "$PACKAGEKIT_CALLER_UID" ]]; then
     user_home=$(getent passwd "$PACKAGEKIT_CALLER_UID" | cut -d: -f6)
-    cp /usr/local/share/applications/owlcms.desktop $user_home/Desktop/owlcms.desktop
+    echo >> $user_home/Desktop/owlcms.desktop <<EOF
+[Desktop Entry]
+Type=Link
+Name=owlcms
+Icon=owlcms
+URL=/usr/local/share/applications/owlcms.desktop
+EOF
+  chmod +x $user_home/Desktop/owlcms.desktop
   else
     echo "No user found to create the desktop file" > /tmp/owlcms.log
     printenv >> /tmp/owlcms.log
