e83c68de323caa6858db6422418cf3729698d5a9
Jean-François Lamy <jf@jflamy.dev>
2025-05-08 21:30:11 -0400

    owlcms-launcher v2.4.1

--- dist/after_install.sh
+++ dist/after_install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 if [[ -n "$SUDO_USER" ]]; then
   user_home=$(getent passwd "$SUDO_USER" | cut -d: -f6)
-cat > $user_home/Desktop/owlcms.desktop <<EOF
+cat > $user_home/Desktop/owlcms <<EOF
 [Desktop Entry]
 Type=Link
 Name=owlcms
 Icon=owlcms
 URL=/usr/local/share/applications/owlcms.desktop
 EOF
-  chmod +x $user_home/Desktop/owlcms.desktop
+  chmod +x $user_home/Desktop/owlcms
 else
   if [[ -n "$PACKAGEKIT_CALLER_UID" ]]; then
     user_home=$(getent passwd "$PACKAGEKIT_CALLER_UID" | cut -d: -f6)
@@ -19,7 +19,7 @@ Name=owlcms
 Icon=owlcms
 URL=/usr/local/share/applications/owlcms.desktop
 EOF
-  chmod +x $user_home/Desktop/owlcms.desktop
+  chmod +x $user_home/Desktop/owlcms
   else
     echo "No user found to create the desktop file" > /tmp/owlcms.log
     printenv >> /tmp/owlcms.log
