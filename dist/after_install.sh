bd87d48dfc39a0213ce5728487b601db05253585
Jean-François Lamy <jf@jflamy.dev>
2025-05-08 21:16:44 -0400

    fix the here document

--- dist/after_install.sh
+++ dist/after_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ -n "$SUDO_USER" ]]; then
   user_home=$(getent passwd "$SUDO_USER" | cut -d: -f6)
-  echo >> $user_home/Desktop/owlcms.desktop <<EOF
+cat > $user_home/Desktop/owlcms.desktop <<EOF
 [Desktop Entry]
 Type=Link
 Name=owlcms
@@ -12,7 +12,7 @@ EOF
 else
   if [[ -n "$PACKAGEKIT_CALLER_UID" ]]; then
     user_home=$(getent passwd "$PACKAGEKIT_CALLER_UID" | cut -d: -f6)
-    echo >> $user_home/Desktop/owlcms.desktop <<EOF
+cat > $user_home/Desktop/owlcms.desktop <<EOF
 [Desktop Entry]
 Type=Link
 Name=owlcms
