2f74101d1b48233a980d81a3525159adf3bc4359
Jean-François Lamy <jf@jflamy.dev>
2026-03-10 18:26:11 -0400

    use 24 LTS for node

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Default release tag (can be overridden by first CLI argument)
-TAG="3.3.0-rc02"
+TAG="3.3.0-rc03"
 # =============================================================================
 # Release script for owlcms-controlpanel
 # Uses workflow_dispatch trigger with tag input parameter
