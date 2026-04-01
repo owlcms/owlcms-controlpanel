a981ab0d3350dca08cdba136d30c401542549051
Jean-François Lamy <jf@jflamy.dev>
2026-04-01 12:15:03 -0400

    Release 3.3.3

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Default release tag (can be overridden by first CLI argument)
-TAG="3.3.2"
+TAG="3.3.3"
 # =============================================================================
 # Release script for owlcms-controlpanel
 # Uses workflow_dispatch trigger with tag input parameter
